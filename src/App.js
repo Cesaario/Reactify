e41cc2513feaa044fe15de21dcf6c0c3d993cf65
Cesaario <gs.cesario1998@gmail.com>
2020-02-14 19:57:21 -0300

    Versão de testes finalizada

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
-import React, {useState, useEffect} from 'react';
-import Spotify from 'spotify-web-api-js';
+import React, { useState, useEffect } from "react";
+import Spotify from "spotify-web-api-js";
 
 const s = new Spotify();
 
 function App() {
-
   const [state, setState] = useState({token: ''});
-  const [intervalo, setIntervalo] = useState(0);
-  //const [params, setParams] = useState('');
+  const [player, setPlayer] = useState({});
 
   useEffect(() => {
-    const {access_token} = getHashParams();
+    const { access_token } = getHashParams();
     setState({
       token: access_token,
       deviceId: "",
@@ -21,52 +19,107 @@ function App() {
       albumName: "Album Name",
       playing: false,
       position: 0,
-      duration: 0,
+      duration: 0
     });
   }, []);
 
   useEffect(() => {
     if (state.token !== "") {
-      console.log('entrou');
-      const interv = setInterval(checkForPlayer, 1000);
-      console.log(interv);
-      setIntervalo(interv);
+      setState({...state, loggedIn: true});
+      setTimeout(checkForPlayer, 1000, state.token);
     }
   }, [state.token]);
 
-  function checkForPlayer() {
-    const { token } = state;
-  
-    console.log("checando");
+  useEffect(() => {
+    transferPlaybackHere();
+  }, [state.deviceId]);
+
+  function checkForPlayer(authToken) {
     if (window.Spotify !== null) {
       const player = new window.Spotify.Player({
         name: "Reactify",
-        getOAuthToken: cb => { cb(token); },
+        getOAuthToken: cb => {
+          cb(authToken);
+        }
       });
-      // this.createEventHandlers();
+      createEventHandlers(player);
+      console.log("Conectado");
       player.connect();
-      console.log(intervalo);
-      clearInterval(intervalo);
+      setPlayer(player);
+    } else {
+      setTimeout(checkForPlayer, 1000, authToken);
+    }
+  }
+  
+  function createEventHandlers(player) {
+    player.on('initialization_error', e => { console.error(e); });
+    player.on('authentication_error', e => {
+      console.error(e);
+      setState({...state, loggedIn: false});
+    });
+    player.on('account_error', e => { console.error(e); });
+    player.on('playback_error', e => { console.error(e); });
+  
+    // Playback status updates
+    player.on('player_state_changed', estado => { onStateChanged(estado); });
+  
+    // Ready
+    player.on('ready', async data => {
+      let { device_id } = data;
+      console.log("Let the music play on!");
+      setState({ ...state, deviceId: device_id });
+    });
+  }
+
+  function transferPlaybackHere() {
+    const { deviceId, token } = state;
+    fetch("https://api.spotify.com/v1/me/player", {
+      method: "PUT",
+      headers: {
+        authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        "device_ids": [ deviceId ],
+        "play": false,
+      }),
+    });
+  }
+
+  function onStateChanged(estado){
+    console.log(estado);
+    if(estado != null){
+      const { current_track } = estado.track_window;
+      console.log(current_track);
+      const { name } = current_track; 
+      console.log(name);
+      setState({...state, trackName: name});
     }
   }
 
-  function getHashParams(){
+  function toggle(){
+    player.togglePlay();
+  }
+
+  function getHashParams() {
     var hashParams = {};
-    var e, r = /([^&;=]+)=?([^&;]*)/g,
-        q = window.location.hash.substring(1);
-    while ( e = r.exec(q)) {
-       hashParams[e[1]] = decodeURIComponent(e[2]);
+    var e,
+      r = /([^&;=]+)=?([^&;]*)/g,
+      q = window.location.hash.substring(1);
+    while ((e = r.exec(q))) {
+      hashParams[e[1]] = decodeURIComponent(e[2]);
     }
     return hashParams;
   }
 
   return (
     <div className="App">
-      <a href='http://localhost:8888'>
+      <a href="http://localhost:8888">
         <button>Login</button>
       </a>
       <div>Tocando: {state.trackName}</div>
       <div>token: {state.token}</div>
+      <button onClick={toggle}>Play/Pause</button>
     </div>
   );
 }
