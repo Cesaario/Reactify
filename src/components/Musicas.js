73d98476bf3504dd722c184366751117d0b9ece4
Cesaario <gs.cesario1998@gmail.com>
2020-03-27 19:40:18 -0300

    Começo da base de minhas músicas

--- src/components/Musicas.js
+++ src/components/Musicas.js
@@ -1,5 +1,9 @@
 import React, { useState } from 'react'
 import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import Divider from '@material-ui/core/Divider';
+import PlayCircleFilledIcon from '@material-ui/icons/PlayCircleFilled';
+import IconButton from '@material-ui/core/IconButton'; 
 
 import './Musicas.css'
 
@@ -9,7 +13,25 @@ function Musicas(){
 
     return(
         <div className='containerMusica'>
-            <Button className='botao'>Minhas Musicas</Button>
+            <Button className='botao' onClick={ () => { setDialogoMusicas(true) }}>Minhas Musicas</Button>
+            <Dialog hideBackdrop PaperProps={{style: {backgroundColor: 'transparent', boxShadow: 'none', minWidth: '80vw'}}} 
+                    open={dialogoMusicas} 
+                    onClose={ () => {setDialogoMusicas(false)} }>
+                <div className='conteudoDialogo'>
+                    <div className='containerTitulo'>
+                        <h1 className='tituloDialogo'>Minhas músicas</h1>
+                    </div>
+                    
+                    <div className='containerMusicas'>
+                    <div className='itemMusica'>
+                            <IconButton>
+                                <PlayCircleFilledIcon style={{ fontSize: 40, fill: '#dddddd'}}></PlayCircleFilledIcon>
+                            </IconButton>
+                            <span>Reapers - Muse</span>
+                        </div>
+                    </div>
+                </div>
+            </Dialog>
         </div>
     );
 
